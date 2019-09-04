1803ee89ae9d9d3642212dcdc55ff09549d2ff85
Marlon <marlonlira3@gmail.com>
2019-09-04 17:14:25 -0300

    criação do usercontrol SmsUsc e alguns ajustes

---
+++ Warden/Components/Controls/SmsUsc.ascx.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web.UI.WebControls;
+using Warden.Components.Common;
+using Warden.Models;
+using Warden.Persistences;
+
+namespace Warden.Components.Controls
+{
+    public partial class SmsUsc : BaseUsc
+    {
+        private DataTable UserTable { get; set; }
+        protected override void OnLoad(EventArgs e) {
+            base.OnLoad(e);
+            Loading();
+            btnPesquisar.OnClick += new ButtonUsc.OnClickEvent(BtnPesquisar_OnClick);
+            btnEnviar.OnClick += new ButtonUsc.OnClickEvent(BtnEnviar_OnClick);
+        }
+
+        private void BtnEnviar_OnClick() {
+            Enviar();
+        }
+
+        private void BtnPesquisar_OnClick() {
+            LoadTable();
+        }
+
+        private void Enviar() {
+            SmsPst Sms;
+            try {
+                Sms = new SmsPst();
+                Sms.Title = txtTitle.Text;
+                Sms.Text = txtText.Text;
+                Sms.Status = "AT";
+                Sms.SelectedSendType = SmsPst.SendType.Multiple;
+
+                if (ddSendType.SelectedValue == "1")
+                    Sms.SelectedSendType = SmsPst.SendType.Simple;
+                else if (ddSendType.SelectedValue == "2")
+                    Sms.SelectedSendType = SmsPst.SendType.Multiple;
+
+                if (ddGateway.SelectedValue == "1")
+                    Sms.SelectedAPI = SmsPst.APIs.SmsFast;
+                else if (ddGateway.SelectedValue == "2") {
+                    Sms.SelectedAPI = SmsPst.APIs.FacilitaSms;
+                    Sms.Sender = new Sender() {
+                        User = "hi.academia.newton",
+                        Pass = "hi.academia.newton123"
+                    };
+                }
+
+                if (String.IsNullOrEmpty(txtNumberList.Text)) {
+                    foreach (DataRow Row in UserTable.Rows) {
+                        Sms.Recipient = new Recipient() {
+                            Name = Convert.ToString(Row["name"]),
+                            PhoneNumber = Convert.ToString(Row["PhoneNumber"])
+                        };
+
+                        Sms.Send();
+                    }
+                } else {
+                    Sms.Recipient = new Recipient() {
+                        Name = Convert.ToString("Lista"),
+                        PhoneNumber = txtNumberList.Text
+                    };
+
+                    Sms.Send();
+                }
+
+            } catch (Exception Except) {
+                
+            }
+        }
+
+        public void Loading() {
+            ddRecipient.ItemList = new List<ListItem>() {
+                new ListItem { Text = "Selecione uma opção", Value="0" },
+                new ListItem {Text = "Aluno", Value = "1"},
+                new ListItem {Text = "Visitante", Value = "2"}
+            };
+
+            ddType.ItemList = new List<ListItem>() {
+                new ListItem { Text = "Selecione uma opção", Value="0" },
+                new ListItem {Text = "ShortCode", Value = "1"},
+                new ListItem {Text = "LongCode", Value = "2"},
+                new ListItem {Text = "Flash", Value = "3"}
+            };
+
+            ddType.SelectedValue = "2";
+            ddType.ReadOnly = true;
+
+            ddCompany.ItemList = new List<ListItem>() {
+                new ListItem { Text = "Selecione uma opção", Value="0" },
+                new ListItem {Text = "Empresa 1", Value = "1"},
+                new ListItem {Text = "Empresa 2", Value = "2"},
+                new ListItem {Text = "Empresa 3", Value = "3"}
+            };
+
+            ddGateway.ItemList = new List<ListItem>() {
+                new ListItem { Text = "Selecione uma opção", Value="0" },
+                new ListItem {Text = "SmsFast", Value = "1"},
+                new ListItem {Text = "FacilitaMovel", Value = "2"}
+            };
+
+            ddSendType.ItemList = new List<ListItem>() {
+                new ListItem { Text = "Selecione uma opção", Value="0" },
+                new ListItem {Text = "Simples", Value = "1"},
+                new ListItem {Text = "Multiplo", Value = "2"}
+            };
+
+
+            ddRecipient.LoadDataSource();
+            ddType.LoadDataSource();
+            ddCompany.LoadDataSource();
+            ddSendType.LoadDataSource();
+            ddGateway.LoadDataSource();
+        }
+
+        private DataTable GetData() {
+            // DataTable de teste
+            DataTable table = new DataTable();
+            table.Columns.Add("Dosage", typeof(int));
+            table.Columns.Add("Drug", typeof(string));
+            table.Columns.Add("Patient", typeof(string));
+            table.Columns.Add("Date", typeof(string));
+
+            // Here we add five DataRows.
+            table.Rows.Add(25, "Indocin", "David", DateTime.Now.ToShortDateString());
+            table.Rows.Add(50, "Enebrel", "Sam", DateTime.Now.ToShortDateString());
+            table.Rows.Add(10, "Hydralazine", "Christoff", DateTime.Now.ToShortDateString());
+            table.Rows.Add(21, "Combivent", "Janet", DateTime.Now.ToShortDateString());
+            table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now.ToShortDateString());
+            table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now.ToShortDateString());
+            table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now.ToShortDateString());
+            table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now.ToShortDateString());
+            table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now.ToShortDateString());
+            table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now.ToShortDateString());
+            table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now.ToShortDateString());
+            table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now.ToShortDateString());
+            table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now.ToShortDateString());
+            return table;
+        }
+
+        private void LoadTable() {
+            tblMessage.TableColumns = new List<BaseTableUsc.TableColumn>() {
+                new BaseTableUsc.TableColumn(){ Name = "Dosage", Text ="Dosagem"},
+                new BaseTableUsc.TableColumn(){ Name = "Drug", Text ="Remedio"},
+                new BaseTableUsc.TableColumn(){ Name = "Patient", Text ="Paciente"},
+                new BaseTableUsc.TableColumn(){ Name = "Date", Text ="Data"}
+            };
+
+            tblMessage.DataSource = GetData();
+        }
+
+    }
+}
---
+++ Warden/Components/Controls/SmsUsc.ascx.designer.cs
@@ -0,0 +1,204 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace Warden.Components.Controls {
+    
+    
+    public partial class SmsUsc {
+        
+        /// <summary>
+        /// Controle txtTitle.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.TextInputUsc txtTitle;
+        
+        /// <summary>
+        /// Controle txtText.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.TextAreaUsc txtText;
+        
+        /// <summary>
+        /// Controle txtNumberList.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.TextAreaUsc txtNumberList;
+        
+        /// <summary>
+        /// Controle lblType.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.LabelUsc lblType;
+        
+        /// <summary>
+        /// Controle ddType.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.DropdownUsc ddType;
+        
+        /// <summary>
+        /// Controle lblRecipient.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.LabelUsc lblRecipient;
+        
+        /// <summary>
+        /// Controle ddRecipient.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.DropdownUsc ddRecipient;
+        
+        /// <summary>
+        /// Controle lblCompany.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.LabelUsc lblCompany;
+        
+        /// <summary>
+        /// Controle ddCompany.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.DropdownUsc ddCompany;
+        
+        /// <summary>
+        /// Controle lblSendType.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.LabelUsc lblSendType;
+        
+        /// <summary>
+        /// Controle ddSendType.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.DropdownUsc ddSendType;
+        
+        /// <summary>
+        /// Controle lblGateway.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.LabelUsc lblGateway;
+        
+        /// <summary>
+        /// Controle ddGateway.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.DropdownUsc ddGateway;
+        
+        /// <summary>
+        /// Controle LabelUsc1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.LabelUsc LabelUsc1;
+        
+        /// <summary>
+        /// Controle txtDataInicial.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.TextInputUsc txtDataInicial;
+        
+        /// <summary>
+        /// Controle LabelUsc2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.LabelUsc LabelUsc2;
+        
+        /// <summary>
+        /// Controle txtDataFinal.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.TextInputUsc txtDataFinal;
+        
+        /// <summary>
+        /// Controle tblMessage.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.TableUsc tblMessage;
+        
+        /// <summary>
+        /// Controle btnPesquisar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.ButtonUsc btnPesquisar;
+        
+        /// <summary>
+        /// Controle btnEnviar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.ButtonUsc btnEnviar;
+        
+        /// <summary>
+        /// Controle btnCancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::Warden.Components.Common.ButtonUsc btnCancelar;
+    }
+}
